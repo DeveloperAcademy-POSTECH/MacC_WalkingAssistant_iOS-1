beafc0e039b9fc29d9c963e3b8c6647512f9ed6d
KimRaymond <megaflops@naver.com>
2022-10-09 11:14:14 +0900

    Apply swiping actions throughout the functions
    ### Motivation
    - Need to implement swiping actions throughout the separate view/pages that contain only one separate function
    
    ### Key Changes
    - FunctionCells.swift, SwipingController.swift files are created.
    - The previous code that implemented the three buttons of app functions has been turned to annotation. The annotated codes will be removed when it is no longer needed to be maintained.
    
    ### To Reviewers
    - See FunctionCells.swift, SwipingController.swift files.

--- Oculo/AppDelegate.swift
+++ Oculo/AppDelegate.swift
@@ -10,9 +10,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let swipingController = SwipingController(collectionViewLayout: layout)
         // Override point for customization after application launch.
         return true
     }
