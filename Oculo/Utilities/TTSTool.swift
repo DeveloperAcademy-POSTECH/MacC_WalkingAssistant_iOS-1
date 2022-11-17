9b6b8d6e173ab1b490da049bd572371764c3454c
huhuthuhut <40786679+huhuthuhut@users.noreply.github.com>
2022-11-17 15:14:16 +0900

    [Feat] VoiceOver가 문자열을 읽도록 변경
    - VoiceOver를 켰을 경우 VoiceOver가 문자열을 읽습니다.

--- Oculo/Utilities/TTSTool.swift
+++ Oculo/Utilities/TTSTool.swift
@@ -7,6 +7,7 @@
 //
 
 import AVFoundation
+import UIKit
 
 public class TTSTool {
     /// TTS 기능의 소리 제어
@@ -17,14 +18,20 @@ public class TTSTool {
 
     /// String을 입력 받아 TTS 수행
     func speak(_ string: String) {
-        let utterance = AVSpeechUtterance(string: string)
-        utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
-        utterance.rate = speakingRate
-        utterance.volume = speakingVolume
+        // VoiceOver와 충돌을 방지하기 위해 VoiceOver가 문장을 읽어 주는 것으로 변경하였습니다.
+        if UIAccessibility.isVoiceOverRunning {
+            UIAccessibility.post(notification: .announcement, argument: string)
+        } else {
+            // 일반적으로 디버깅 용은 TTS가 읽음.
+            let utterance = AVSpeechUtterance(string: string)
+            utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+            utterance.rate = speakingRate
+            utterance.volume = speakingVolume
 
-        /// synthesizer에서 현재 말하는 중인 경우 즉시 중단한다. (소리가 겹쳐서 들리는 현상 방지)
-        stopSpeak()
-        synthesizer.speak(utterance)
+            /// synthesizer에서 현재 말하는 중인 경우 즉시 중단한다. (소리가 겹쳐서 들리는 현상 방지)
+            stopSpeak()
+            synthesizer.speak(utterance)
+        }
     }
 
     func stopSpeak() {
